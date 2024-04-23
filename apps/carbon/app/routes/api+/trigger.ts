9342fcdabea294caa31f96619b4e074ec7586f9d
Kartik Subramani <knsubramani@gmail.com>
2024-04-23 11:55:26 -0400

    Reenabled export of files in jobs.server directory

--- apps/carbon/app/routes/api+/trigger.ts
+++ apps/carbon/app/routes/api+/trigger.ts
@@ -1,13 +1,10 @@
-//import { createRemixRoute } from "@trigger.dev/remix";
 import { triggerClient } from "~/lib/trigger.server";
 import serverRuntime from "@remix-run/server-runtime";
 import type { ActionFunction } from "@remix-run/node";
 
 // Remix will automatically strip files with side effects
 // So you need to *export* your Job definitions like this:
-//export * from "~/jobs.server";
-
-// export const { action } = createRemixRoute(triggerClient);
+export * from "~/jobs.server";
 
 export const action: ActionFunction = async ({ request }) => {
   const response = await triggerClient.handleRequest(request);
